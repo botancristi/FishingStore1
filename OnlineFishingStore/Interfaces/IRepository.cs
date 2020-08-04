0b0007bfe0325f9b44505d8ee92a2ecc264cbef0
Cristi Botan <botancristi@gmail.com>
2020-08-04 21:15:10 +0300

    create a generic repository

---
+++ OnlineFishingStore/Interfaces/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OnlineFishingStore.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        void Delete(TEntity entityToDelete);
+
+        void Delete(object id);
+
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProprieties = "");
+
+        TEntity GetByID(object id);
+
+        IEnumerable<TEntity> GetWithRawSql(string query,
+            params object[] parameters);
+
+        void Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ OnlineFishingStore/Repository/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlineFishingStore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OnlineFishingStore.Repository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal ApplicationContext context;
+        internal DbSet<TEntity> dbSet;
+
+    }
+}
